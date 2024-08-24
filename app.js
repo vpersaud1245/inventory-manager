ccf396f418be086f4b402bc28a7bc6e04e9d13f9
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-08-24 15:11:21 -0400

    feat: Add productsRouter to app

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const app = express();
+const productsRouter = require("./routes/products");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
+app.use("/products", productsRouter);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => `Express app listening on port ${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Express app listening on port ${PORT}`));
 
+/**
+ * JUST COMPLETED
+ * DB IS POPULATED, SCRIPT WAS MADE TO POPULATE IT
+ * DB IS NAMED top_inventory
+ */
 /**
  * TODO
  * Products (CRUD)
