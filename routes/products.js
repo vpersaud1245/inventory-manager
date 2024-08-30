6a7c52b2ef0ab3d816484abc66aa632e0ad7bbe2
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-08-30 14:24:25 -0400

    feat: Add getProductById controller to route

--- routes/products.js
+++ routes/products.js
@@ -3,5 +3,6 @@ const productsRouter = express.Router();
 const productsController = require("../controllers/productsController");
 
 productsRouter.get("/", productsController.getAllProducts);
+productsRouter.get("/:productId", productsController.getProductById);
 
 module.exports = productsRouter;
