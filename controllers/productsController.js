b8e01025e3784f1c5a85cf2cfcb8a0191eb29f23
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-08-30 14:23:18 -0400

    feat: Add getProductById function

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -6,4 +6,11 @@ module.exports = {
     res.send(products);
     res.end();
   },
+  getProductById: async (req, res) => {
+    const productId = req.params["productId"];
+    console.log(`...Retreiving product with Id ${productId}`);
+    const product = await db.getProductById(productId);
+    res.send(product);
+    res.end();
+  },
 };
